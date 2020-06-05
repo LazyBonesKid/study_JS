296c46d18cbcce1bf8b74a5f4e5e1203b094860d
Damir Sattarov <dam.sattaroff2017@yandex.ru>
2020-06-05 14:53:47 +0500

    2 урок обязательное задание

--- script/script.js
+++ script/script.js
@@ -1,2 +1,10 @@
-alert('privet');
-alert('poka');
+let 
+money,
+income,
+addExpenses,
+deposite,
+mission = 'Получить 2 балла за задание',
+period;
+
+alert('Здравствуйте, Богдан');
+console.log(mission);
