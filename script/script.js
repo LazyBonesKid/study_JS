142cd4ab35432e6531636650c3ff1ffbcc837780
Damir Sattarov <dam.sattaroff2017@yandex.ru>
2020-06-08 18:26:09 +0500

    3 урок

--- script/script.js
+++ script/script.js
@@ -1,18 +1,59 @@
+'use strict';
+
 let 
-money  = 30000,
-income = 'Продажа арбузов',
-addExpenses = 'Мороженное, гречка, туалетная бумага',
-deposite = true,
+money,
+income,
+addExpenses,
+deposite,
 mission = 100500,
 period = 11;
 
-console.log(typeof money, typeof income, typeof deposite);
-console.log(addExpenses.length);
+
+
+while (!parseInt(money)) {
+    money = prompt('Ваш месячный доход?');
+    if(money == null){
+        alert('Это поле обезательно к заполнению');
+        continue;
+    }
+    if(!money.trim()){
+        alert('пустая строка (или только пробелы)');
+        continue;
+    }
+    if(!Number(money)){
+        alert('Только цифры');
+        continue;
+    }
+    Number(money);
+}
+
+console.log(typeof money);
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+deposite = confirm('Есть ли у вас депозит в банке?');
 console.log('Период равен ' + period + ' месяцев');
 console.log('Цель заработать ' + mission + ' крышек нюка колы');
-
 console.log(addExpenses = addExpenses.toLocaleLowerCase().split(', '));
 
+let 
+expenses1 = prompt('Введите обязательную статью расходов'),
+expenses2 = prompt('Введите обязательную статью расходов'),
+amount1 = prompt('Во сколько это обойдется?'),
+amount2 = prompt('Во сколько это обойдется?'),
+budgetMonth = money - Number(amount1) + Number(amount2);
 
-let budgetDay = money / 30 ;
+console.log('Бюджет на месяц :', budgetMonth);
+console.log('Цель будет достигнута через:', Math.ceil(mission/budgetMonth), 'месяца');
+
+
+let budgetDay = Math.floor(budgetMonth / 30) ;
 console.log(budgetDay);
+
+if (budgetDay >= 1200) {
+    console.log('У вас высокий уровень дохода');
+} else if (budgetDay >= 600 && budgetDay < 1200) {
+    console.log('У вас средний уровень дохода');
+} else if (budgetDay < 600 && budgetDay > 0) {
+    console.log('К сожалению у вас уровень дохода ниже среднего');
+} else if (budgetDay < 0 ) {
+    console.log('Что то пошло не так');
+} 
