7f0024e7d76d45fa08806f5e7a62cb9764c7a7b0
Damir Sattarov <dam.sattaroff2017@yandex.ru>
2020-06-09 22:49:06 +0500

    урок 4

--- script/script.js
+++ script/script.js
@@ -39,37 +39,37 @@ console.log(addExpenses = addExpenses.toLocaleLowerCase().split(', '));
 
 let 
 expenses1 = prompt('Введите обязательную статью расходов'),
-expenses2 = prompt('Введите обязательную статью расходов'),
 amount1 = +prompt('Во сколько это обойдется?', 2500),
+expenses2 = prompt('Введите обязательную статью расходов'),
 amount2 = +prompt('Во сколько это обойдется?', 500);
 
 
 
-const getExpensesMonth = function (amountOne, amountTwo) {
+
+let getExpensesMonth = function (amountOne, amountTwo) {
     return amountOne + amountTwo;
-}
+};
 
-const getAccumulatedMonth = function (money, amountOne , amountTwo) { // попробывал через callback функцию
+let getAccumulatedMonth = function (money, amountOne , amountTwo) { // попробывал через callback функцию
     return money - getExpensesMonth(amountOne, amountTwo);
-}
+};
 
-let  accumulatedMonth = getAccumulatedMonth(money, amount2, amount1);
 console.log('Бюджет на месяц: ', accumulatedMonth);
 
-const getTargetMonth = function (mission, accumulated) {
+let getTargetMonth = function (mission, accumulated) {
     return 'Цель будет достигнута через: ' + Math.ceil(mission/accumulated) + ' месяца';
-}
+};
 
 console.log(getTargetMonth(mission, accumulatedMonth));
 
-const getBudgetDay = function (accumulated) {
+let getBudgetDay = function (accumulated) {
     return Math.floor(accumulatedMonth / 30)
-}
+};
 
 let budgetDay = getBudgetDay(accumulatedMonth);
 console.log('Бюджет на день:', budgetDay);
 
-let getStatusIncome = function(budgetDay) {
+let getStatusIncome = function() {
     if (budgetDay >= 1200) {
         return('У вас высокий уровень дохода');
     } else if (budgetDay >= 600 && budgetDay < 1200) {
