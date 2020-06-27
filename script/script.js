58488b159d4391e2c42b948b79dc43a5a7a870e5
Дамир Саттаров <61625025+LazyBonesKid@users.noreply.github.com>
2020-06-27 17:43:26 +0500

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,5 +1,3 @@
-'use strict';
-
 function DomElement (selector, height, width, bg, frontSize) {
     this.selector  = selector;
     this.height    = height;
@@ -10,42 +8,34 @@ function DomElement (selector, height, width, bg, frontSize) {
 }
 
 DomElement.prototype.domCreate = function () {
-    let style = document.createElement('style');
-    style.type = 'text/css';
-
-    style.innerHTML =`${this.selector} { 
-    height: ${this.height};
-    width:  ${this.width};
-    background: ${this.bg};
-    font-size: ${this.frontSize};
-    `;
-
-    document.getElementsByTagName('head')[0].appendChild(style);
-    
     let 
     selectorDop = this.selector.slice(1),
     docEl;
 
-    if (this.selector.indexOf('.') !== -1) {
+    if (this.selector.indexOf('.') !== -1){
         docEl = document.createElement('div');
         docEl.className = selectorDop;
     }else if (this.selector.indexOf('#') !== -1){
         docEl = document.createElement('p');
         docEl.setAttribute('id', selectorDop);
     }
+    
+    docEl.style.cssText = `
+    height: ${this.height};
+    width:  ${this.width};
+    background: ${this.bg};
+    font-size: ${this.frontSize};
+    `;
 
-    docEl.textContent =  prompt('text');
+    docEl.textContent = prompt('text');
+
     document.body.append(docEl);
-
 };
 
-let div = new DomElement('.black','100px','100px','#00FA9A', '50px');
-let p = new DomElement('#black','50px','30px','#008080', '30px');
+let domEl = new DomElement('.blue','100px','100px','blue', '50px');
+let p = new DomElement('#red','50px','50px','red', '25px');
 
-
-
-div.domCreate();
+domEl.domCreate();
 p.domCreate();
-
 
 
